74c994e8902c3e65db5e8a02fadf71879af88abf
Xperia-45 <ayushkatoch@2008gmail.com>
2026-09-17 23:06:30 +0530

    Clean duplicate project files

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import Welcome from './components/welcome.jsx'
-import statecard from './components/statecard.jsx'
-function App() {
-  return (
-    <Welcome />
-  )
-}
-
-export default App
--- src/components/statecard.jsx
+++
@@ -1,8 +0,0 @@
-function statecard({title}) {
-    <StatCard title="GPA" />
-    return (
-        <div>
-        
-        </div>
-    )
-}
